f875d409820737268a53d03cbed15c853d64f88c
mikenewt <mikejnewt@gmail.com>
2014-04-12 18:07:26 +0800

    Finished animating Ryu.

--- js/app.js
+++ js/app.js
@@ -8,15 +8,28 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// show hadouken and animate it to right of screen
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-120px');
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 		// ryu goes back to ready position
 	})
-})
+});
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
