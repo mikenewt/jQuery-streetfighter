8b84521fd6a4e82e89646cfbf72636ea25839257
mikenewt <mikejnewt@gmail.com>
2014-04-13 21:24:30 +0800

    Finished Ryu, started working on logo/text.
    Wrote doIntro(), preparing to move Ryu stuff into playGame().

--- js/app.js
+++ js/app.js
@@ -24,9 +24,29 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu goes back to ready position
 	})
 });
+$(document).keydown(function(event) {
+	if (event.which == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-cool').show();
+	}
+}).keyup(function(event) {
+	if (event.keyCode == 88) {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	}
+});
+
+function doIntro() {
+	$(document).ready(function() {
+		$('.sf-logo').fadeIn(3500, function() {
+			$(this).fadeOut(1000, function() {
+				$('.text').fadeIn(1500);
+			})
+		});
+	})
+}
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
